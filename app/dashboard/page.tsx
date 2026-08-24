c742f36694cfe45fa35e1d1d4dd40a6a289a496f
Karol <karol.wichmann@outlook.com>
2026-08-24 21:25:52 +0200

    deployment test

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,21 +1,22 @@
-import { Card } from "@/app/ui/dashboard/cards";
-import RevenueChart from "@/app/ui/dashboard/revenue-chart";
-import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
-import { lusitana } from "@/app/ui/fonts";
-import { fetchLatestInvoices, fetchRevenue, fetchCardData } from "../lib/data";
+// import { Card } from "@/app/ui/dashboard/cards";
+// import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+// import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+// import { lusitana } from "@/app/ui/fonts";
+// import { fetchLatestInvoices, fetchRevenue, fetchCardData } from "../lib/data";
 
-export default async function Page() {
-  const [revenue, latestInvoices] = await Promise.all([fetchRevenue(), fetchLatestInvoices()]);;
-  const {
-    totalPaidInvoices,
-    totalPendingInvoices,
-    numberOfInvoices,
-    numberOfCustomers,
-  } = await fetchCardData();
+export default function Page() {
+  // const [revenue, latestInvoices] = await Promise.all([fetchRevenue(), fetchLatestInvoices()]);;
+  // const {
+  //   totalPaidInvoices,
+  //   totalPendingInvoices,
+  //   numberOfInvoices,
+  //   numberOfCustomers,
+  // } = await fetchCardData();
 
   return (
     <main>
-      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+      check
+      {/* <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
@@ -31,7 +32,7 @@ export default async function Page() {
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue} />
         <LatestInvoices latestInvoices={latestInvoices} />
-      </div>
+      </div> */}
     </main>
   );
 }
